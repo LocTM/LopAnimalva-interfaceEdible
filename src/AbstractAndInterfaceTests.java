15fbdc36b8bdfa91c800caccf68a60a74d3fbcd3
LocTM <tranminhloc.anhk@gmail.com>
2023-12-18 16:33:45 +0700

    doneAbstractAndInterfaceTests

--- src/AbstractAndInterfaceTests.java
+++ src/AbstractAndInterfaceTests.java
@@ -9,7 +9,10 @@ public class AbstractAndInterfaceTests {
         animals[1] = new Chicken();
         for (Animal animal : animals) {
             System.out.println(animal.makeSound());
+            if(animal instanceof Chicken){
+                Edible edibler = (Chicken) animal;
+                System.out.println(edibler.howToEat());
+            }
         }
-
     }
 }
